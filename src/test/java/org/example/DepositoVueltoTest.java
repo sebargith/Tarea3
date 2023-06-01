671b34c4772e7e4ef0824a5f499831380395acf1
Naxoegt <jucarvajal2020@udec.cl>
2023-05-31 22:43:42 -0400

    Agregar DepositoVuelto
    Se agrega DepositoVuelto funcional

--- src/test/java/org/example/DepositoVueltoTest.java
+++ src/test/java/org/example/DepositoVueltoTest.java
@@ -2,16 +2,56 @@ package org.example;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DepositoVueltoTest {
 
-    @BeforeEach
-    void setUp() {
+    @Test
+    @DisplayName("Test agregar una moneda")
+    public void testaddUnaMoneda(){
+        System.out.println("AgregaUnaMonedaAlDeposito");
+        DepositoVuelto<Moneda> deposito = new DepositoVuelto<>();
+        Moneda100 moneda100 = new Moneda100();
+
+        deposito.addMoneda(moneda100);  //agrega productos
+
+        assertEquals(moneda100, deposito.getMoneda());  //compara si lo que se saca es lo que agrego
+
     }
 
-    @AfterEach
-    void tearDown() {
+    @Test
+    @DisplayName("Test agregar una moneda null y no obtener nada del deposito")
+    public void testaddMonedaNull(){
+        System.out.println("AgregaUnaMonedaNull");
+        DepositoVuelto<Moneda> deposito = new DepositoVuelto<>();
+        deposito.addMoneda(null);
+        assertNull(deposito.getMoneda());
+
     }
+
+
+    @Test
+    @DisplayName("Test agregar y sacar monedas varias monedas hasta vaciar el deposito")
+    public void testAddandGetMonedas(){
+        System.out.println("Agregaysacamonedas");
+        DepositoVuelto<Moneda> deposito = new DepositoVuelto<>();
+        Moneda100 moneda100a = new Moneda100();
+        Moneda100 moneda100b = new Moneda100();
+        Moneda100 moneda100c = new Moneda100();
+
+
+
+        deposito.addMoneda(moneda100a);
+        deposito.addMoneda(moneda100b);
+        deposito.addMoneda(moneda100c);
+
+        assertEquals(moneda100a, deposito.getMoneda());
+        assertEquals(moneda100b, deposito.getMoneda());
+        assertEquals(moneda100c, deposito.getMoneda());
+        assertNull(deposito.getMoneda());
+    }
+
 }
