e7133ba3008e694d6e18bdb00b68ee86167eadca
sebargith <131008747+sebargith@users.noreply.github.com>
2023-05-31 22:27:29 -0400

    Add empty test classes
    se agregan clases vacias que serviran como modelo para los unit test

---
+++ src/test/java/org/example/DepositoTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepositoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
